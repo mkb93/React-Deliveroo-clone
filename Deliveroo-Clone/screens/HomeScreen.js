6cb466acba55e9395ff8aa9393f963c203ffaec5
marco belahouane <nfk18@hotmail.co.uk>
2023-07-18 21:04:48 +0100

    making the search component look nice only input no function

--- Deliveroo-Clone/screens/HomeScreen.js
+++ Deliveroo-Clone/screens/HomeScreen.js
@@ -1,6 +1,13 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon
+} from "react-native-heroicons/outline"
+import Categories from '../components/Categories'
 
 const HomeScreen = () => {
   const navigation= useNavigation();
@@ -11,15 +18,41 @@ const HomeScreen = () => {
     })
   })
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
       {/* Header */}
-      <View>
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 ">
         <Image source={{
           uri: 'https://links.papareact.com/wru'
          }} 
-         className="h-7 w-7 bg-gray-300 p-4 rounded-full"/>
+         className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+         />
+         <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+         </View>
+         <UserIcon size={35} color='#00CCBB'/>
+    
       </View>
-   
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+         <MagnifyingGlassIcon size={20} color='gray'/>
+         <TextInput 
+         placeholder='Restaurants and cuisines'
+         keyboardType='default'
+         />
+        </View>
+        <AdjustmentsVerticalIcon size={35} color='#00CCBB'/>
+      </View>
+         {/* Body */}
+         <ScrollView>
+          {/* categories */}
+         <Categories/>
+          {/* featured row */}
+         </ScrollView>
     </SafeAreaView>
   )
 }
