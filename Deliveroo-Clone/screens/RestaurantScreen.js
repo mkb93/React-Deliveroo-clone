c2becf86f3bf2ffaf005dc9fde4c758d407cd276
marco belahouane <nfk18@hotmail.co.uk>
2023-07-26 13:41:29 +0100

    add styling to the upper aprt of the restaurant screen

--- Deliveroo-Clone/screens/RestaurantScreen.js
+++ Deliveroo-Clone/screens/RestaurantScreen.js
@@ -2,8 +2,8 @@ import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { urlFor } from '../sanity'
-import { ArrowLeftIcon } from 'react-native-heroicons/solid'
-
+import { ArrowLeftIcon, ChevronRightIcon, MapPinIcon, StarIcon } from 'react-native-heroicons/solid'
+import { QuestionMarkCircleIcon } from 'react-native-heroicons/outline'
 const RestaurantScreen = () => {
   const navigation = useNavigation()
   const {params: {  
@@ -38,9 +38,30 @@ const RestaurantScreen = () => {
       </TouchableOpacity>
     </View>
     <View className="bg-white">
-        <View>
-          <Text>{title}</Text>
+        <View className="px-4 pt-4">
+          <Text className="text-3xl font-bold">{title}</Text>
+          <View className="flex-row space-x-2 my-1">
+            <View className="flex-row items-center space-x-1">
+              <StarIcon color='green' opacity={0.5} size={22} />
+              <Text className='text-xs text-gray-500'>
+                <Text className='text-green-500'>{rating}</Text> - {genre}
+                </Text>
+            </View>
+
+            <View className="flex-row items-center space-x-1">
+              <MapPinIcon color='gray' opacity={0.4} size={22} />
+              <Text className='text-xs text-gray-500'>
+                Nearby {address}
+                </Text>
+            </View>
+          </View>
+          <Text className='text-gray-500 mt-2 pb-4'>{short_description}</Text>
         </View>
+        <TouchableOpacity className='flex-row items-center space-x-2 p-4 border-y border-gray-300'>
+          <QuestionMarkCircleIcon color='gray' opacity={0.6} size={20}  />
+          <Text className='pl-2 flex-1 text-md font-bold'> Have a food allergy?</Text>
+          <ChevronRightIcon color='#00CCBB'/>
+        </TouchableOpacity>
     </View>
     </ScrollView>
   )
