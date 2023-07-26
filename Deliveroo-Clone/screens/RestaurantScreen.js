6ed812d64bc7a30991b4fc34a08fe9f0e38ebbcc
marco belahouane <nfk18@hotmail.co.uk>
2023-07-26 13:06:04 +0100

    set an image dynamically on restaurant screen and have a styled button that doesnt do anything

--- Deliveroo-Clone/screens/RestaurantScreen.js
+++ Deliveroo-Clone/screens/RestaurantScreen.js
@@ -1,6 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
+import { urlFor } from '../sanity'
+import { ArrowLeftIcon } from 'react-native-heroicons/solid'
 
 const RestaurantScreen = () => {
   const navigation = useNavigation()
@@ -24,9 +26,18 @@ const RestaurantScreen = () => {
     }, []);
 
   return (
-    <View>
-      <Text>{title}</Text>
+    <ScrollView>
+    <View className='relative'>
+      <Image source={{
+        uri: urlFor(imgUrl).url()
+      }}
+      className="w-full h-56 bg-gray-300 p-4"
+      />
+      <TouchableOpacity className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full">
+        <ArrowLeftIcon size={20} color="#00CCBB"/>
+      </TouchableOpacity>
     </View>
+    </ScrollView>
   )
 }
 
